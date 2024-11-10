b39c401b16293159a2d5bedfdc3398a5e1f40d72
WissamMU <wessam.2003.ali@gmail.com>
2024-11-10 03:17:43 +0300

    new project - webpack files - base files - navbar - home carousel

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/css/bootstrap.rtl.min.css"
+import '../sass/style.scss'
+import "jquery/dist/jquery"
+
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: './js/index_bundle.js',
+    },
+
+    module: {
+        rules: [
+            
+            {
+                test: /\.(sass|css|scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    "css-loader",
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer
+                                ]
+                            }
+                        }
+                    },
+                    "sass-loader",
+                ],
+
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "./images/[name][ext]",
+                },
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: "./fonts/[name][ext]",
+                },
+            },            {
+                test: require.resolve("jquery"),
+                loader: "expose-loader",
+                options: {
+                    exposes: ["$", "jQuery"],
+                },
+            },
+            
+        ],
+    },
+    
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        devMiddleware: {
+            writeToDisk: true,
+        },
+        //compress: true,
+        port: 9000,
+        open: false,
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/style.css",
+        }),
+
+
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+};
